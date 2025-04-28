1740582969baa1d5b3b88354379e2cb2ee230470
Kontent Kreation LLC <kontentkreationllc@Kontents-MacBook-Pro.local>
2025-04-28 10:28:38 -0700

    fix: updated API URL to news.mewskhabar.com

--- src/api/getNews.ts
+++ src/api/getNews.ts
@@ -1,17 +1,17 @@
 export async function getNews() {
-    try {
-      const res = await fetch('https://cornflowerblue-moose-538317.hostingersite.com/wp-json/wp/v2/posts', {
-        headers: {
-          'Accept': 'application/json',
-        },
-      });
-  
-      if (!res.ok) {
-        throw new Error('Failed to fetch news');
-      }
-      return await res.json();
-    } catch (error) {
-      console.error('Error fetching news:', error);
-      throw error;
+  try {
+    const res = await fetch('https://news.mewskhabar.com/wp-json/wp/v2/posts', {
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch news');
     }
+    return await res.json();
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    throw error;
   }
+}
